fb7f3218342a86aa041bc934076bd3401fa352e5
ligoudan <935777655@qq.com>
2021-05-31 17:05:51 +0800

    push commit

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ console.log("test1");
 console.log("test2");
 console.log("test3");
 console.log("hot-fix branch commit");
-console.log("master branch commit2");
 console.log("hot-fix branch commit2");
+console.log("push commit");
